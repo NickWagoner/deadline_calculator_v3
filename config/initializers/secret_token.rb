c1a25d8ec24cdc6733c42f060f437087519aa166
Nicholas J. Wagoner <nicholas.wagoner@gmail.com>
2014-02-09 22:59:51 -0600

    Created basic deadline calculator.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeadlineCalculatorV3::Application.config.secret_key_base = '889f7fbbde7794b9a8811c2c4e7fd6fb7c7de0b2522852f1b5ff5d0dfbb96ff5f22bdb12fef7a620fe80e313b9d204bfbe4dc1a82121c83537e2f9b42da8e9c0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeadlineCalculatorV3::Application.config.session_store :cookie_store, key: '_deadline_calculator_v3_session'
